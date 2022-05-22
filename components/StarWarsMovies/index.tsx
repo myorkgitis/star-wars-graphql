2096b2a030b007b6490d88ccb2dade11eb730116
Matthew Yorkgitis <matt@itisconsultingllc.com>
2022-05-22 08:46:39 -0400

    Switched useQuery to useLazyQuery to delay fetching on SW Movies Page

--- components/StarWarsMovies/index.tsx
+++ components/StarWarsMovies/index.tsx
@@ -1,8 +1,9 @@
 import {Button, Form, Input, Pagination} from 'antd';
-import {gql, useQuery} from "@apollo/client";
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
 import StarWarsMovieTable from "./StarWarsMovieTable";
 import {ThreeDots} from "react-loader-spinner";
 
+// TODO add search variable to query to filter on the movie titles
 const FETCH_MOVIES_QUERY = gql`
     query FetchMovies {
         films(order: releaseDate_ASC) {
@@ -26,8 +27,15 @@ const StarWarsMovies = () => {
     // https://parseapi.back4app.com/graphql
 
     // Make call to SW API here
+    // Use lazy query to delay fetching
+    // https://www.apollographql.com/docs/react/data/queries/#manual-execution-with-uselazyquery
+    //
     // If there is data, show the table, otherwise show a loader
-    const { loading, error, data } = useQuery(FETCH_MOVIES_QUERY)
+    const [ getMovies, { loading, error, data } ] = useLazyQuery(FETCH_MOVIES_QUERY, {
+        variables: {
+            search: ""
+        }
+    })
 
     console.log(data)
 
@@ -50,8 +58,8 @@ const StarWarsMovies = () => {
                     <Input />
                 </Form.Item>
             </Form>
-            <Pagination defaultCurrent={1} total={50} />
-            <Button type={"primary"}>Load Data</Button>
+
+            <Button type={"primary"} onClick={() => getMovies()}>Load Data</Button>
             {loading ?
                 <ThreeDots color="#00BFFF" height={80} width={80} />
                 :
