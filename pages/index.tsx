41c15e3697b54143b266d8d38255af3d5df5d3e3
Matthew Yorkgitis <matt@itisconsultingllc.com>
2022-05-22 07:43:02 -0400

    Added layout to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import StarWarsMovies from "../components/StarWarsMovies";
 
+import {Layout, Typography} from 'antd';
+
+const {Header, Footer, Content} = Layout;
+const {Title, Paragraph, Link} = Typography;
+
 const Home: NextPage = () => {
-  return (
-    <div>
-        <StarWarsMovies />
-    </div>
-  )
+
+    // TODO fix the header title spacing
+    return (
+        <>
+            <Layout>
+                <Header>
+                    <Title style={{color: "white"}}>Star Wars Movies 🚀💥🍿</Title>
+                </Header>
+                <Content style={{padding: '50px'}}>
+                    <StarWarsMovies/>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    <Paragraph>Built by Matt Yorkgitis</Paragraph>
+                    <Paragraph>Uses the <Link href={"https://www.back4app.com/database/davimacedo/swapi-star-wars-api"}>StarWars GraphQL API</Link></Paragraph>
+                </Footer>
+            </Layout>
+        </>
+    )
 }
 
 export default Home
